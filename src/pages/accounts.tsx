504fd11a3f046cd8ed9ae5ed5b750f7ae438fd3e
Hong Minhee <hong@minhee.org>
2024-10-27 10:50:50 +0900

    Send `Delete(Person)` to followings

--- src/pages/accounts.tsx
+++ src/pages/accounts.tsx
@@ -10,6 +10,7 @@ import {
   generateCryptoKeyPair,
   getActorHandle,
   isActor,
+  type Recipient,
 } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import { PromisePool } from "@supercharge/promise-pool";
