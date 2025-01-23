75d93ec7bac436e8f22720d93734cd8428fb627b
Omar Salah <omar.salah1597@gmail.com>
2025-01-23 16:06:53 +0200

    Refactor generateOutbox to restructure replies object, improving data encapsulation and clarity in the outbox generation process.

--- src/entities/outbox.ts
+++ src/entities/outbox.ts
@@ -113,7 +113,11 @@ async function generateOutbox(actor: Actor, baseUrl: string | URL) {
           to: to.length > 0 ? to : undefined,
           cc: cc.length > 0 ? cc : undefined,
           tags: tags.length > 0 ? tags : undefined,
-          replies: replies,
+          replies: {
+            id: replies?.id,
+            totalITems: replies?.totalItems,
+            items: replies?.getItems
+          },
           likes: likes,
           shares: shares,
         });
